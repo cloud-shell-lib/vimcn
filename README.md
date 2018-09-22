7eb6ddb0c2600fbaa0a75902219a01fb75282b10
xaoxuu <xaoxuu@gmail.com>
2018-09-23 00:24:14 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# vimcn
-a shell script to upload files to img.vim-cn.com
+# vim-cn.sh
+一个上传图片到 [img.vim-cn.com](img.vim-cn.com) 的脚本。
+
+## 使用方法
+
+打开终端，`cd` 到存放图片的文件夹，输入这行命令：
+
+```bash
+curl -s https://raw.githubusercontent.com/xaoxuu/vim-cn.sh/master/upload_all.sh | sh
+```
+
+然后就会开始上传所有图片，并打印出图片的URL。
+
+> 请不要上传一些奇奇怪怪的图片。
+
---
+++ upload_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function upload(){
+	url=`curl -s -F "name=@${FILENAME}" https://img.vim-cn.com/ 2>&1`
+	printf "\033[32m${FILENAME}\033[0m 已上传至: \033[32m${url}\033[0m\n" && open $url
+}
+
+function start(){
+	# 遍历每一个podspec文件
+	echo "> 开始上传..."
+	for FILENAME in *.{png,jpg,ico,gif,ico,svg,tiff,webp,pdf,mp3,mp4,zip}
+	do
+		if [ -r "$FILENAME" ];then 
+	 		upload &
+	    fi 
+	done
+	wait
+	echo "> 全部上传完毕！"
+}
+
+start
